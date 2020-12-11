5e2bd986be43da8708ecd9051aaeb1b6f531c9ba
Anindo21 <74434247+Anindo21@users.noreply.github.com>
2020-12-11 19:54:22 +0100

    Updating

--- Webapp/app.py
+++ Webapp/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
+from datetime import datetime
+
 app = Flask(__name__)
 app.secret_key = "mndkfjkdsfj"
 
@@ -9,8 +11,7 @@ app.config['MYSQL_PASSWORD'] = 'anindo'
 app.config['MYSQL_DB'] = 'lab3'
 
 mysql = MySQL(app)
-user = 111
-id = 12
+user = 110
 
 @app.route('/')
 @app.route('/view_main')
@@ -26,8 +27,14 @@ def new_course():
         text = request.form["des"]
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO course(name, description, enrollmentkey, free_places, creator) VALUES(%s, %s, %s, %s, %s)", (name, text, key, place, user))
-        mysql.connection.commit()
+        if key:
+            cur.execute("INSERT INTO course(name, description, enrollmentkey, free_places, creator) VALUES(%s, %s, %s, %s, %s)", (name, text, key, place, user))
+            mysql.connection.commit()
+        else:
+            cur.execute(
+                "INSERT INTO course(name, description, free_places, creator) VALUES(%s, %s, %s, %s)",
+                (name, text, place, user))
+            mysql.connection.commit()
         cur.close()
         flash("Course created successfully", "success")
         return redirect(url_for("view_main"))
@@ -38,27 +45,66 @@ def new_course():
 def view_course():
 
     cur = mysql.connection.cursor()
-    res = cur.execute("SELECT * FROM course WHERE creator = %s", (user,))
+    res = cur.execute("SELECT * FROM view_course WHERE user = %s", (user,))
     if res>0:
         value = cur.fetchall()
 
-    res1 = cur.execute("SELECT * FROM course WHERE creator != %s", (user,))
-    if res1 > 0:
-        values = cur.fetchall()
+    cur.execute("select * from view_course where ID not in (SELECT ID FROM view_course WHERE user = %s)", (user,))
+
+    values = cur.fetchall()
+
     cur.close()
 
     return render_template("view_course.html", value=value, values=values)
 
-@app.route('/view_course_detail')
-def view_course_detail():
-
+@app.route('/view_course/<int:cid>')
+def view_course_detail(cid):
+    infos = [] # list of course id from enroll
     cur = mysql.connection.cursor()
-    res = cur.execute("select * from course_detail where ID = %s", (id,))
+    res = cur.execute("select * from course_detail where ID = %s", (cid,))
     if res>0:
         value = cur.fetchall()
+
+    cur.execute("select * from enroll where user = %s", (user,))
+    info = cur.fetchall()
     cur.close()
 
-    return render_template("view_course_detail.html", value=value)
+    for i in info:
+        infos.append(int(i[1]))
+
+    return render_template("view_course_detail.html", value=value, infos=infos)
+
+@app.route('/new_enroll/<int:cid>', methods=['GET', 'POST'])
+def new_enroll(cid):
+    cur = mysql.connection.cursor()
+    cur.execute("select name, enrollmentkey from course where ID = %s", (cid,))
+    info = cur.fetchall()
+    name = info[0][0]
+    key = info[0][1]
+
+    if request.method == 'POST':
+        if key != None:
+            ekey = request.form["ek"]
+            if key == int(ekey):
+                cur.execute(
+                    "INSERT INTO enroll(user, course, date_of_entry) VALUES(%s, %s, %s)",
+                    (user, cid, datetime.today().strftime('%Y-%m-%d')))
+                mysql.connection.commit()
+                flash("Welcome to the course!!!", "success")
+                return redirect(url_for("view_course"))
+            else:
+                flash("Wrong enrollment key", "danger")
+        else:
+            cur.execute(
+                "INSERT INTO enroll(user, course, date_of_entry) VALUES(%s, %s, %s)",
+                (user, cid, datetime.today().strftime('%Y-%m-%d')))
+            mysql.connection.commit()
+            flash("Welcome to the course!!!", "success")
+            return redirect(url_for("view_course"))
+    cur.close()
+
+    return render_template("new_enroll.html", name=name, key=key)
+
 
 if __name__ == '__main__':
     app.run()
